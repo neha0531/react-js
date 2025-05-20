be632107bcad48e68d7235c3e784f9659aaeb423
neha0531 <pnehareddy4444@gmail.com>
2025-05-20 17:11:17 +0530

    feat: setup Vite + Tailwind with Atomic folder structure and JSX demo

---
+++ src/components/PromoBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ const PromoBanner = () => {
+  return (
+    <div className="bg-red-400 text-black text-center py-3 text-3xl font-bold">
+      🔥Supper Summer Sale! Up to 50% off!
+    </div>
+  );
+};
+
+export default PromoBanner;
